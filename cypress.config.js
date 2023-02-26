5360ea242ef82bde0e958e9c036e8b0f95457366
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2023-02-26 20:27:40 +0100

    add env url + spec googleSearch display title

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        env: {
+            BASE_URL: "https://google.fr",
+        },
+        chromeWebSecurity: false,
     },
-  },
 });
--- cypress/e2e/googleSearch.cy.js
+++ cypress/e2e/googleSearch.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import "cypress-plugin-xhr-toggle";
+
+describe("google search", () => {
+    it("should display three first title", () => {
+        cy.visit(Cypress.env("BASE_URL"));
+        cy.get("#L2AGLb").click();
+        cy.get('input[name="q"]').type("vadesecure");
+        cy.get("form").submit();
+        cy.get("#search a h3").then((title) => {
+            for (let i = 0; i < 3; i++) {
+                cy.log(`${i + 1} title result : ${title[i].innerText}`);
+            }
+        });
+    });
+});
